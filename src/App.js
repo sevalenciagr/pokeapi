7ab7f12cec9c3854cf7da913ee4371640d02c4a2
sevalenciagr <sevalenciagr@gmail.com>
2022-12-21 14:10:31 -0500

    Prueba Buho

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
 
+
+
+
 function App() {
+
+  fetch('https://pokeapi.co/api/v2/ability/?limit=20&offset=20')
+  .then((response) => response.json())
+  .then((data) => console.log(data));
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <article>
+    <h1></h1>
+    <span></span>
+   </article>
   );
 }
 
